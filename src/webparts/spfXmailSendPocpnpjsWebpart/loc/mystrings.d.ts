892ddbe7f3f598a795f9ff803db294fd1a3a8535
Shuvodip Ray <shuvodip@pphackathonteam5.onmicrosoft.com>
2021-08-11 10:40:04 +0530

    first commit

---
+++ src/webparts/spfXmailSendPocpnpjsWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfXmailSendPocpnpjsWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfXmailSendPocpnpjsWebpartWebPartStrings' {
+  const strings: ISpfXmailSendPocpnpjsWebpartWebPartStrings;
+  export = strings;
+}
